c9beac685b256666a01f19cbf233ff80e41b7a10
pavanpk1707 <148988297+pavanpk1707@users.noreply.github.com>
2023-10-26 10:46:24 +0000

    Ternary Operators

---
+++ 23_Ternary_Operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int findMax(int x, int y)
+{
+    return (x > y) ? x : y;
+}
+int main()
+{
+    // ternary operator = shortcut to if/else when assigning/returning a value
+    // (condition) ? value if true : value if false
+
+    int max = findMax(-1, 0);
+    printf("The maximum value is %d\n", max);
+
+    return 0;
+}
