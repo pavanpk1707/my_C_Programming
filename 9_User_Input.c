d713bcce654b77ce8d47cad7c6b081ebc9d9fe19
pavanpk1707 <148988297+pavanpk1707@users.noreply.github.com>
2023-10-26 08:19:58 +0000

    User Input

--- 9_User_Input.c
+++ 9_User_Input.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     char name[25];
     int age;
     printf("\nWhat's your name?: ");
-    scanf("%s", name);
+    // scanf("%s", name);
+    fgets(name, 25, stdin);
+    name[strlen(name) - 1] = '\0';
 
     printf("How old are you?: ");
     scanf("%d", &age);
