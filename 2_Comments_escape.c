f4fe77cebf12074e864a7b5162dbdc5e3ea3894c
pavanpk1707 <148988297+pavanpk1707@users.noreply.github.com>
2023-10-26 03:47:29 +0000

    Comments and escape sequence

---
+++ 2_Comments_escape.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    // This is a single line comment
+    /*
+    This
+    is
+    a
+    multiline
+    comment
+    */
+    printf("I like Pizza!\n");
+
+    /* escape sequence = character combination consisting of a backslash
+    followed by a letter or combination of digits. They specify actions
+    within a line or string of text
+    \n = newline
+    \t = tab
+    */
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("\"I Like Pizza\" - Abraham Lincoln probably\n");
+    return 0;
+}
