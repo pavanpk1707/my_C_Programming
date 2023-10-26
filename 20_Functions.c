e383056eb2d75d8c5ca6cf94718195ac3fdcc2af
pavanpk1707 <148988297+pavanpk1707@users.noreply.github.com>
2023-10-26 10:25:02 +0000

    Functions

---
+++ 20_Functions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void wishes();
+int main()
+{
+    wishes();
+    wishes();
+    wishes();
+
+    return 0;
+}
+
+void wishes()
+{
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday dear...YOU!");
+    printf("\nHappy birthdat to you!\n");
+}
