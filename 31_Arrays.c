68f3d4acfc340ab29831a86764d099307005a943
pavanpk1707 <148988297+pavanpk1707@users.noreply.github.com>
2023-10-27 03:09:14 +0000

    Arrays

---
+++ 31_Arrays.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // array = a data structure that can store many values of the same data type
+
+    double prices[] = {3.4, 6.8, 8.9};
+
+    printf("$%.2lf\n", prices[2]);
+
+    return 0;
+}
